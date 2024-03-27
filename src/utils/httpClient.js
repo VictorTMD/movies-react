9668fdc20f470eb14d6faa871d832cb15009009f
Victor Morales <victor_teo_17@hotmail.com>
2024-03-27 19:10:16 +0100

    se corrige error de la variable del token

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,11 +1,10 @@
 const API = process.env.REACT_APP_API;
-// const TOKEN = process.env.REACT_APP_TOKEN;
+const TOKEN = process.env.REACT_APP_APITOKEN;
 
 export async function get(path) {
   const result = await fetch(API + path, {
     headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNGQ0ZWE3MjgwOWJhMTY0NzcyNDhhMDI3OWE5N2M3ZiIsInN1YiI6IjY1ZjQxZDc5Mzg1MjAyMDE4NmUzMWZjOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OdFcKGX615dosYQ5_VnV8W_GaqsMZPQD2ju1TrozVRw',
+      Authorization: 'Bearer ' + TOKEN,
       'Content-Type': 'application/json;charset=utf-8',
     },
   });
